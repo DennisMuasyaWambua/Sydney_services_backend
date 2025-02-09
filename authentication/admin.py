6254db0343cfe864bef9a39efd1362e936758bba
Dennis wambua <muasyathegreat4@gmail.com>
2025-02-09 20:24:20 +0300

    postgres database added and service provider and service endpoints created

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(Services)
+admin.site.register(ServiceProvider)
+admin.site.register(Reviews)
